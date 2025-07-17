54a47d3a6ed617c59ef5bfda3e762d4c6cde86cc
Jiehoon Lee <jiehoonn@bu.edu>
2025-07-16 20:56:42 -0400

    feat: new skylark font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen">
-      <h1 className="text-4xl font-bold">Landing Page</h1>
+      <h1 className="font-skylark text-4xl font-bold">Landing Page</h1>
     </div>
   );
 }
